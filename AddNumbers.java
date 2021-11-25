a2b1874ab4897c552c3599f59be4f230b901fe30
raviteja <94172814+badagala@users.noreply.github.com>
2021-11-25 09:12:04 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,16 @@
+package addingtwonumbers;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+     int x=5;
+     int y=6;
+     int z=9;
+     int a=x+y+z;
+     System.out.println(a);
+    
+     
+
+	}
+
+}

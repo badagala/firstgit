2fc1d3ad3a8912961238cd9db5d10236367d212b
raviteja <94172814+badagala@users.noreply.github.com>
2022-05-29 23:26:00 +0530

    Add files via upload

---
+++ ChangingPlace.java
@@ -0,0 +1,22 @@
+package com.mmg.pdfformat;
+
+import java.io.File;
+import java.util.List;
+
+import javax.print.DocFlavor.URL;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageFitWidthDestination;
+
+public class ChangingPlace {
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
